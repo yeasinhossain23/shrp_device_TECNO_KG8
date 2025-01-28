150e719dda6736038c780241bc79199500080121
yeasinhossain23 <yeasintahsin000@gmail.com>
2025-01-29 00:40:33 +0600

    first commit

---
+++ twrp_KG8.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from KG8 device
+$(call inherit-product, device/TECNO/KG8/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := KG8
+PRODUCT_NAME := twrp_KG8
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KG8
+PRODUCT_MANUFACTURER := TECNO
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_kg8_h698-user 11 RP1A.200720.011 449400 release-keys"
+
+BUILD_FINGERPRINT := TECNO/KG8-GL/TECNO-KG8:11/RP1A.200720.011/231010V601:user/release-keys
