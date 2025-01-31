e1e89b00c9ce30b58a582ca5f03ec21ab2c97cbf
Yeasin Hossain <99527781+yeasinhossain23@users.noreply.github.com>
2025-02-01 00:57:33 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_neon.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_neon.mk
 
-COMMON_LUNCH_CHOICES := twrp_neon-eng
-
+COMMON_LUNCH_CHOICES := \
+	twrp_neon-user \
+	twrp_neon-userdebug \
+	twrp_neon-eng
+#
