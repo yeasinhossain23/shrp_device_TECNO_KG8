af79ecde299107cce49fa2c6701cc85dcccba47d
Yeasin Hossain <99527781+yeasinhossain23@users.noreply.github.com>
2025-01-31 15:10:02 +0600

    Update twrp_neon.mk

--- twrp_neon.mk
+++ twrp_neon.mk
@@ -4,7 +4,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+PRODUCT_RELEASE_NAME := neon
+DEVICE_PATH := device/tecno/neon
+
+
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
